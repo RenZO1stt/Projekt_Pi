5a19423e588a3db46ca9e15cbe16e387ba813221
RenZO1stt <rermacora@unipu.hr>
2025-11-09 22:34:27 +0100

    Komentar

--- Pi_projekt/src/oglasi.js
+++ Pi_projekt/src/oglasi.js
@@ -3,21 +3,20 @@ import { ref, reactive } from "vue";
 import { db, doc, onSnapshot } from "@/firebase.js";
 
 
-// Make oglasi a reactive array directly
 const oglasi = reactive([]);
 const currentUser = ref(null);
 
-// Reference the single document /ads/myAd
+
 const docRef = doc(db, "ads", "myAd");
 
 // Real-time listener
 onSnapshot(docRef, (docSnap) => {
   if (docSnap.exists()) {
     const data = docSnap.data();
-    // Replace array contents reactively
+   //Updatea content
     oglasi.splice(0, oglasi.length, ...(data.array || []));
   } else {
-    oglasi.splice(0, oglasi.length); // clear if no doc
+    oglasi.splice(0, oglasi.length); 
   }
 });
 
